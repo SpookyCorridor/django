9e5c3c87f2b3e7e432c70b0a71f7cf48b76da7a5
Dylan <invision33@gmail.com>
2015-03-24 19:47:58 -0500

    Updated url.py for lists and superlists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
-    # url(r'^admin/', include(admin.site.urls)),
+url(r'^$', 'lists.views.home_page', name='home'), url(r'^lists/', include('lists.urls')),
+# url(r'^admin/', include(admin.site.urls)),
 )
