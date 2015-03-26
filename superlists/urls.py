af1d50b5e9d01c044c051738664bc99c347d5ba7
Dylan <invision33@gmail.com>
2015-03-26 14:02:59 -0500

    change static file location and dirpath

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('',
-url(r'^$', 'lists.views.home_page', name='home'), url(r'^lists/', include('lists.urls')),
+url(r'^$', 'lists.views.home_page', name='home'),
+url(r'^lists/', include('lists.urls')),
 # url(r'^admin/', include(admin.site.urls)),
 )
